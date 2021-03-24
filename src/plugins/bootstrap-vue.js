48e6b9969a89d69de022bb405635f2b7e7604718
wawanneutron <wawanneutron1331@gmail.com>
2021-03-24 10:21:48 +0700

    all

---
+++ src/plugins/bootstrap-vue.js
@@ -0,0 +1,10 @@
+import Vue from 'vue'
+
+import BootstrapVue from 'bootstrap-vue'
+import Bootstrap from 'bootstrap'
+
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+
+
+Vue.use(BootstrapVue, Bootstrap)
